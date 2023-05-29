7e5b6d93ba9cb6fc4500cc18c35a919f886cb8ed
JokerLi04 <134508609+JokerLi04@users.noreply.github.com>
2023-05-29 19:10:39 +0800

    second commit

---
+++ zifuchuan.py
@@ -0,0 +1,21 @@
+print('I\'m ok')
+# \ zhuan3 yi4
+
+print('I\'m learning\npythom.')
+# \n huan4 hang2
+
+print('\\\n\\')
+
+print('\\\t\\')
+# \t zhi4 ciao3 fu2 - 8 ge4 kong4 ge2
+
+print(r'\\\t\\')
+# r' bu4 zhuan3 yi4 (\ shi1 xiao4)
+
+print('''line1
+line2
+line3''')
+# '''...''' xiang1 dang1 yu2 \n (... wei2 shu1 ru4 nei4 rong2)
+
+print(r'''hello\n
+world''')
